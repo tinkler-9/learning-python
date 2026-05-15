7132d6fc68915d63aa0cc40e4288d9f2b4b165f7
Tatiana Iakovleva <t.neckrasova@gmail.com>
2026-05-15 21:11:45 +0200

    zip reversed

--- t1/day10.py
+++ t1/day10.py
@@ -1,1 +1,19 @@
-help(zip)
+#help(zip)
+a = ("John", "Charles", "Mike")
+b = ("Jenny", "Christy")
+
+x = list(zip(range(1, 4), a, b))
+for y in x:
+  print(y)
+#use the tuple() function to display a readable version of the result:
+
+print(tuple(x))
+
+alph = ["a", "b", "c", "d"]
+ralph = reversed(alph)
+print(type(ralph))
+for i in ralph:
+  print(i)
+for i in ralph:
+  print(i)
+
