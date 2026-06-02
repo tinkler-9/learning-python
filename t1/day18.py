3260b2967fa5277bbe373a5b2209f5b23ff883fd
Tatiana Iakovleva <t.neckrasova@gmail.com>
2026-06-02 21:17:28 +0200

    weight plot

--- t1/day18.py
+++ t1/day18.py
@@ -1,13 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-heights = np.genfromtxt("https://raw.githubusercontent.com/gagolews/" +
-    "teaching-data/master/marek/nhanes_adult_female_height_2020.txt")
-counts, bins, __not_important = plt.hist(heights, bins=11,
-    color="lightgray", edgecolor="red")
-plt.ylabel("Count")
-#plt.show()
-print(bins)
-print(counts)
+
 
 weight = np.genfromtxt("https://raw.githubusercontent.com/gagolews/teaching-data/master/marek/nhanes_adult_female_weight_2020.txt")
 counts, bins, __not_important = plt.hist(weight, bins=11,
